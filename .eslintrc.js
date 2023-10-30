090aafd0dc942ce62946f7a8068c954ec991aa5b
m4ggiee <121451790+m4ggiee@users.noreply.github.com>
2023-10-30 17:42:08 +0200

    third commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "sourceType": 'module',
+        "ecmaVersion": 2018
+    },
+    "rules": {
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,41 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+import * as sassLibrary from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(sassLibrary);
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import minify from 'gulp-uglify';
+import eslint from 'gulp-eslint';
 
-gulp.task('sass', function() {
+const compileSass = () => {
   return gulp.src('sass/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'));
-});
+};
 
-gulp.task('watch', function() {
-  gulp.watch('sass/*.scss', ['sass']);
-});
+const watchSass = () => {
+  gulp.watch('sass/*.scss', compileSass);
+};
 
-gulp.task('default', ['sass', 'watch']);
+const minifyAndConcatJs = () => {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(minify())
+    .pipe(gulp.dest('dist/js'));
+};
+
+const lintCode = () => {
+  return gulp.src('src/js/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format());
+};
+
+const watchJs = () => {
+  gulp.watch('src/js/*.js', gulp.series(lintCode, minifyAndConcatJs));
+};
+
+gulp.task('sass', compileSass);
+gulp.task('watch-sass', watchSass);
+gulp.task('default', gulp.series('sass', 'watch-sass'));
+gulp.task('js', minifyAndConcatJs);
+gulp.task('lint', lintCode);
+gulp.task('watch-js', watchJs);
